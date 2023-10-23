4ec6efd023710020278f432e4f72b9e3409a5ae8
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-23 13:51:13 +0300

    Add files via upload

---
+++ for-3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace for_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.txtSayilar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.ForeColor = System.Drawing.Color.Red;
+            this.btnEkle.Location = new System.Drawing.Point(48, 109);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(115, 34);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSayilar.ForeColor = System.Drawing.Color.Red;
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.ItemHeight = 20;
+            this.lbSayilar.Location = new System.Drawing.Point(209, 25);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(116, 284);
+            this.lbSayilar.TabIndex = 1;
+            // 
+            // txtSayilar
+            // 
+            this.txtSayilar.Location = new System.Drawing.Point(48, 25);
+            this.txtSayilar.Multiline = true;
+            this.txtSayilar.Name = "txtSayilar";
+            this.txtSayilar.Size = new System.Drawing.Size(115, 34);
+            this.txtSayilar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(82, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GALATASARAY👑";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(353, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSayilar);
+            this.Controls.Add(this.lbSayilar);
+            this.Controls.Add(this.btnEkle);
+            this.ForeColor = System.Drawing.Color.Tan;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.TextBox txtSayilar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
